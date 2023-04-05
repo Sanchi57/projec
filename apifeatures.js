080291621fd18281888688e104f182a777c4b6e7
Sanchi57 <129848793+Sanchi57@users.noreply.github.com>
2023-04-05 22:01:44 +0545

    Add files via upload

---
+++ apifeatures.js
@@ -0,0 +1,51 @@
+class ApiFeatures {
+    constructor(query, queryStr){
+        this.query = query;
+        this.queryStr = queryStr
+    }
+    search(){
+        const keyword = this.queryStr.keyword ? {
+            name:{
+                //mongodb operaters
+                $regex:this.queryStr.keyword,
+                $options: "i",  //by making this options "i" small it makes case insensetive while searching.
+
+            },
+        }:
+        {};
+
+        this.query = this.query.find({...keyword }); //sending the keyword we had made by using $regex
+        return this;
+    }
+
+    filter(){
+        const queryCopy = {...this.queryStr}
+        
+        //Removing some fields for category
+        const removeFields = ["keyword","page","limit"];
+
+        removeFields.forEach(key=>delete queryCopy[key]);
+
+        //Filter for price and Rating
+
+        
+        let queryStr = JSON.stringify(queryCopy);
+        queryStr = queryStr.replace(/\b(gt|gte|lt|lte)\b/g,key =>`$${key}`);
+
+        
+        this.query = this.query.find(JSON.parse(queryStr));
+        return this;
+    }
+    pagination(resultPerPage) {
+        const currentPage = Number(this.queryStr.page) || 1;
+    
+        const skip = resultPerPage * (currentPage - 1);
+    
+        this.query = this.query.limit(resultPerPage).skip(skip);
+    
+        return this;
+      }
+}
+
+
+module.exports = ApiFeatures;
